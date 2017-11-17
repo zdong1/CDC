d335e18a64d31b9d3cdaee01416b6e1cf85db91a
Zhihang Dong <zhdong2@gmail.com>
2017-11-17 14:54:21 -0800

    Update clus.sh

--- clus.sh
+++ clus.sh
@@ -1,6 +1,13 @@
-# To load the 8G data, need 24G CPU...
+# To load the 8G data, need 24G CPU..., see --mem-per-cpu
 srun --pty --time=1200 --mem-per-cpu=10240 --partition=largemem /bin/bash
 module load R
 R
-> demog<-read.csv(file="./MDC/Data/RawData/demographics.csv",header=FALSE,sep=",")
+# All CDC Files are tab-delimited...
+> demog<-read.csv(file="./MDC/Data/RawData/demographics.csv",header=FALSE,sep="\t")
 > gps<-read.csv(file="./MDC/Data/RawData/gps.csv",header=FALSE,sep="\t")
+# Test Everything is right...
+> head(gps, n=10)
+# Rename the column...
+> colnames(gps.s)<-c("db_key","time","long","lat","deltat")
+> head(gps.s, n=10)
+
