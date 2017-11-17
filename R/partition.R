ceede18fd17a4415255f6c2875edb3da20f4b5da
Zhihang Dong <zhdong2@gmail.com>
2017-11-17 14:58:53 -0800

    Create partition.R

---
+++ R/partition.R
@@ -0,0 +1,17 @@
+# All CDC Files are tab-delimited...
+###################################
+# Load the Required Package 
+###################################
+library("dplyr")
+library("sp")
+library("ggplot2")
+
+# Partition into small files, and filter out redundant information
+
+demog<-read.csv(file="./MDC/Data/RawData/demographics.csv",header=FALSE,sep="\t")
+gps<-read.csv(file="./MDC/Data/RawData/gps.csv",header=FALSE,sep="\t")
+# Test Everything is right...
+head(gps, n=10)
+# Rename the column...
+colnames(gps.s)<-c("db_key","time","long","lat","deltat")
+head(gps.s, n=10)
