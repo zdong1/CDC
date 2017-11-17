4bd73690fca7588b3887bb8e7e7a4716b4d4c8ca
Zhihang Dong <zhdong2@gmail.com>
2017-11-17 15:21:02 -0800

    Update partition.R

--- R/partition.R
+++ R/partition.R
@@ -1,12 +1,13 @@
-# All CDC Files are tab-delimited...
 ###################################
 # Load the Required Package 
+# Actually the packages below won't be used lol...
 ###################################
 library("dplyr")
 library("sp")
 library("ggplot2")
 
 # Partition into small files, and filter out redundant information
+# Note all of the CDC data are tab-delimited
 
 demog<-read.csv(file="./MDC/Data/RawData/demographics.csv",header=FALSE,sep="\t")
 gps<-read.csv(file="./MDC/Data/RawData/gps.csv",header=FALSE,sep="\t")
@@ -16,5 +17,5 @@ head(gps, n=10)
 colnames(gps.s)<-c("db_key","time","long","lat","deltat")
 head(gps.s, n=10)
 # Save this into RData, the size shrinked to 1G --> 150M 
-save(gps.s,file="./gps.s.Rda")
+save(gps.s,file="./MDCRes/gps.s.Rda")
 quit()
