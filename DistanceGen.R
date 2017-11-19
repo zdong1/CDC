61e6e67f943f80f209a6d412ef6829a612a9561a
biggyd <zdong@Zhihangs-MacBook-Pro.local>
2017-11-18 17:27:07 -0800

    add a trial

--- DistanceGen.R
+++ DistanceGen.R
@@ -6,3 +6,7 @@ library("gmapsdistance")
 # Sample data: 3700 observations of a person in CDC (very small partition of a partition...)
 # load("d0_one.Rdata") -- See Github repository
 
+get.api.key()
+
+trial1<-gmapsdistance(origin="46.5255+6.65218", destination = "46.5240+6.61600",mode="walking")
+trial1
