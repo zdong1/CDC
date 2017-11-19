a1e3720b350b971cae2327bd844bdf01a21d8dd0
biggyd <zdong@Zhihangs-MacBook-Pro.local>
2017-11-18 16:59:15 -0800

    Generate distance based on drive distance using Google Map API

---
+++ DistanceGen.R
@@ -0,0 +1,8 @@
+# CDC Distance Generator 
+# If you don't have this:
+# install.packages("gmapsdistance")
+library("gmapsdistance")
+
+# Sample data: 3700 observations of a person in CDC (very small partition of a partition...)
+# load("d0_one.Rdata") -- See Github repository
+
