96a648ddcf4fc48ea524733d97e7d4eda2f026d6
bigz <zhdong2@gmail.com>
2017-11-20 15:14:07 -0800

    Update joinTables.R

--- R/joinTables.R
+++ R/joinTables.R
@@ -1,7 +1,7 @@
 library(dplyr)
 load(file="./MDCRes/gps.s.Rda")
 load(file="./MDCRes/rec.s.Rda")
-
+load(file="./MDCRes/dem.Rda") # demographics file
 # JOIN TABLES 
 rec.l<- rec.s %>% left_join(gps.s)
 # Oops, I found records come with lots of NAs, let's remove them
@@ -16,3 +16,13 @@ rec.l<-subset(rec.l,long!="NA")
 
 # save the linked file as CSV format
 save(rec.l,file="./MDCRes/rec.l.csv")
+
+#
+rec.ld<- rec.l %>% left_join(dem)
+# Joining, by = "personid"
+# Check to make sure the size remain unchanged after the left join (unmatched fields become 'NA')
+> length(rec.l$long)
+> [1] 11758820
+> length(rec.ld$long)
+> [1] 11758820
+
