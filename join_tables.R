efb78505b167b4ccc738c7064be93c6d5f511af3
biggyd <zdong@D-69-91-132-4.dhcp4.washington.edu>
2017-11-19 20:09:01 -0800

    save linked files

--- join_tables.R
+++ join_tables.R
@@ -4,4 +4,14 @@ load(file="./MDCRes/rec.s.Rda")
 
 # JOIN TABLES 
 rec.l<- rec.s %>% left_join(gps.s)
+# Oops, I found records come with lots of NAs, let's remove them
+rec.l<-subset(rec.l,long!="NA")
+# > length(rec.l$db_key)
+# [1] 11758820
+# We get approx. 65,000 unique gps records per person...
+# > length(unique(rec.l$personid))
+# [1] 185
+# > 11758820/185
+# [1] 63561.19
+
 
