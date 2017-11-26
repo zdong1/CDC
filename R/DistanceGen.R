3d8de5d646c675ba8f408513a44331b393557ec1
bigz <zhdong2@gmail.com>
2017-11-26 15:03:48 -0800

    Update DistanceGen.R

--- R/DistanceGen.R
+++ R/DistanceGen.R
@@ -1,50 +1,44 @@
+##########################################################################################################
 # CDC Distance Generator 
+# Zhihang Dong
+# Version 1.4
+# 11/26/2017
+##########################################################################################################
 # If you don't have this:
 # install.packages("gmapsdistance")
 library("gmapsdistance")
 library("geosphere")
 library(sp)
 require(sp)
-
-
-
-
-# Sample data: 10900 observations of a person in CDC (very small partition of a partition...)
-# load("d0_one.Rdata") -- See Github repository
-
-get.api.key()
-d0_one<-read.csv(file="person_5578.csv",header=FALSE, sep=",")
+# Sample data: 3600 observations of a person in CDC (very small partition of a partition...)
+# load("d1.Rdata") -- See Github repository
+# If the use of gmaps function get solved
+# get.api.key()
 # trial1<-gmapsdistance(origin="46.5255+6.65218", destination = "46.5240+6.61600",mode="walking")
 # trial1
 # trial1$Distance
 # trim the dataset
-d0<-d0_one[c(1,4:6)]
-colnames(d0)<-c("db_key","time","lat","lon")
-# sort the data set by time
-d0<-d0[order(d0$time),]     # or attach(d0); d0<-do[order(time)]
-
-coordinates(d0)<- ~ lon+lat
-proj4string(d0) <- CRS("+proj=longlat +datum=WGS84")
-
+##########################################################################################################
 # TODO: Solve - Google API returned an error: You have exceeded your daily request quota for this API.
 # pair<-paste(d0$lon[1],"+",d0$lat[1],sep="") # sep="" removes the white space
-
 # attach(d0)
 # for (i in 1:(nrow(d0)-1)){
 #   d0$origin[i]<-paste(d0$lon[i],"+",d0$lat[i],sep="")
 #   d0$destination[i]<-paste(d0$lon[i+1],"+",d0$lat[i+1],sep="")
 #   d0$dist[i]<-gmapsdistance(origin=d0$origin[i], destination=d0$destination[i], mode="walking")$Distance
 # }
-
-
-
+###########################################################################################################
+# Crow flies Distance Version - Less Ideal, but mathematically fine...
+###########################################################################################################
+# Prepare the Data
 d1_one<-read.csv(file="person_5479.csv",header=FALSE, sep=",")
 d1<-d1_one[c(1,4:6)]
 colnames(d1)<-c("db_key","time","lat","lon")
+# sort the data set by time
 d1<-d1[order(d1$time),]     # or attach(d1); d1<-d1[order(time),]
 
 
-## No more bugs
+## Main Functions
 genDist<-function(raw){
   coordinates(raw)<- ~ lon+lat
   proj4string(raw) <- CRS("+proj=longlat +datum=WGS84")
@@ -62,9 +56,7 @@ genDist<-function(raw){
   }
   raw
 }
-
-
-
+# Generate this New Dataset
 d1.new<-genDist(raw=d1)
 #############################################
 # replicate this method on another person
@@ -72,6 +64,6 @@ d1.new<-genDist(raw=d1)
 # TODO: use a function to read files
 # DONE 
 #############################################
-
+# Examine the Dataset
 summary(d1.new@data$sum.dists)
 
