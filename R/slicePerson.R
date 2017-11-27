bf386d0f7ccf3ead5e565337a3bf73aeec8e6bf7
biggyd <zdong@D-108-179-131-19.dhcp4.washington.edu>
2017-11-26 21:32:21 -0800

    A simple program to slice df by PersonID

---
+++ R/slicePerson.R
@@ -0,0 +1,16 @@
+# Split Persons Programs
+# Load the data and sort the column you want to slide on
+load(file="./MDCRes/rec.ld.Rda")
+rec.ld<-rec.ld[order(rec.ld$personid),]
+
+# Slice the data
+s <- setNames(split(rec.ld, rec.ld$personid), paste0("person", unique(rec.ld$personid)))
+list2env(s, globalenv())
+# Now, we have 183 datasets called 'personxxxx' based on his/her personid
+# Check and see if it is right
+# > summary(person5448$personid)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 5448    5448    5448    5448    5448    5448 
+# > summary(person6272$personid)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 6272    6272    6272    6272    6272    6272 
