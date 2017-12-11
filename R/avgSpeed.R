34fe05227d74b8917db7ac5578b2081b7351f327
biggyd <zdong@Zhihangs-MacBook-Pro.local>
2017-12-11 05:53:19 -0800

    update the weekday/weekend plot

--- R/avgSpeed.R
+++ R/avgSpeed.R
@@ -5,17 +5,10 @@ setUpWeek <- function(person){
   test = person@data
   test$wkdy = weekdays(as.Date(test$time))
   test$week = ceiling(test$sum.t/168)
-  monday = test[which(test$wkdy=='Monday'),]
-  tues = test[which(test$wkdy=='Tuesday'),]
-  weds = test[which(test$wkdy=='Wednesday'),]
-  thurs = test[which(test$wkdy=='Thursday'),]
-  friday = test[which(test$wkdy=='Friday'),]
-  sats = test[which(test$wkdy=='Saturday'),]
-  sunday = test[which(test$wkdy=='Sunday'),]
   test
 }
 
-df = setUpWeek(p.5943)
+daf = setUpWeek(p.5940)
 
 # This draws the function
 drawTrends <- function(flat){
@@ -55,55 +48,42 @@ drawTrends(df)
 
 
 ### Weekday Seasonality Plot (Very rough)
-days1 <- NULL
-days2 <- NULL
-days3 <- NULL
-days4 <- NULL
-days5 <- NULL
-days6 <- NULL
-days7 <- NULL
 
-for (i in 1:108){
-  days1[i]<-(rev(monday[which(monday$week==i),10])[1] - monday[which(monday$week==i),10][1])/
-    (rev(monday[which(monday$week==i),11])[1] - monday[which(monday$week==i),11][1])
+WeekPlot <-function(daf){
+  days1 = days2 = days3 = days4 = days5 = days6 = days7 = NULL
+  monday = daf[which(daf$wkdy=="Monday"),]
+  tues = daf[which(daf$wkdy=="Tuesday"),]
+  weds = daf[which(daf$wkdy=="Wednesday"),]
+  thurs = daf[which(daf$wkdy=="Thursday"),]
+  friday = daf[which(daf$wkdy=="Friday"),]
+  sats = daf[which(daf$wkdy=="Saturday"),]
+  sunday = daf[which(daf$wkdy=="Sunday"),]
+  for (i in 1:max(daf$week)){
+    days1[i]<-(rev(monday[which(monday$week==i),10])[1] - monday[which(monday$week==i),10][1])/
+      (rev(monday[which(monday$week==i),11])[1] - monday[which(monday$week==i),11][1])
+    days2[i]<-(rev(tues[which(tues$week==i),10])[1] - tues[which(tues$week==i),10][1])/
+      (rev(tues[which(tues$week==i),11])[1] - tues[which(tues$week==i),11][1])
+    days3[i]<-(rev(weds[which(weds$week==i),10])[1] - weds[which(weds$week==i),10][1])/
+      (rev(weds[which(weds$week==i),11])[1] - weds[which(weds$week==i),11][1])
+    days4[i]<-(rev(thurs[which(thurs$week==i),10])[1] - thurs[which(thurs$week==i),10][1])/
+      (rev(thurs[which(thurs$week==i),11])[1] - thurs[which(thurs$week==i),11][1])
+    days5[i]<-(rev(friday[which(friday$week==i),10])[1] - friday[which(friday$week==i),10][1])/
+      (rev(friday[which(friday$week==i),11])[1] - friday[which(friday$week==i),11][1])
+    days6[i]<-(rev(sats[which(sats$week==i),10])[1] - sats[which(sats$week==i),10][1])/
+      (rev(sats[which(sats$week==i),11])[1] - sats[which(sats$week==i),11][1])
+    days7[i]<-(rev(sunday[which(sunday$week==i),10])[1] - sunday[which(sunday$week==i),10][1])/
+      (rev(sunday[which(sunday$week==i),11])[1] - sunday[which(sunday$week==i),11][1])
+  }
+  
+  plot(days1,pch=19,cex=0.7,col="gray",ylim=c(0,30),xlab="Week",ylab="Speed (km/h)",xlim=c(0,max(daf$week)))
+  points(days2,pch=19,cex=0.7, col="gray",ylim=c(0,20))
+  points(days3,pch=19,cex=0.7,col="gray",ylim=c(0,20))
+  points(days4,pch=19,cex=0.7,col="gray",ylim=c(0,20))
+  points(days5,pch=19,cex=0.7,col="darkturquoise",ylim=c(0,20))
+  points(days6,pch=19,cex=0.7,col="red",ylim=c(0,20))
+  points(days7,pch=19,cex=0.7,col="orange",ylim=c(0,20))
+  abline(h=8,lty=2)
 }
 
-for (i in 1:108){
-  days2[i]<-(rev(tues[which(tues$week==i),10])[1] - tues[which(tues$week==i),10][1])/
-    (rev(tues[which(tues$week==i),11])[1] - tues[which(tues$week==i),11][1])
-}
+WeekPlot(daf)
 
-for (i in 1:108){
-  days3[i]<-(rev(weds[which(weds$week==i),10])[1] - weds[which(weds$week==i),10][1])/
-    (rev(weds[which(weds$week==i),11])[1] - weds[which(weds$week==i),11][1])
-}
-
-for (i in 1:108){
-  days4[i]<-(rev(thurs[which(thurs$week==i),10])[1] - thurs[which(thurs$week==i),10][1])/
-    (rev(thurs[which(thurs$week==i),11])[1] - thurs[which(thurs$week==i),11][1])
-}
-
-for (i in 1:108){
-  days5[i]<-(rev(friday[which(friday$week==i),10])[1] - friday[which(friday$week==i),10][1])/
-    (rev(friday[which(friday$week==i),11])[1] - friday[which(friday$week==i),11][1])
-}
-
-for (i in 1:108){
-  days6[i]<-(rev(sats[which(sats$week==i),10])[1] - sats[which(sats$week==i),10][1])/
-    (rev(sats[which(sats$week==i),11])[1] - sats[which(sats$week==i),11][1])
-}
-
-for (i in 1:108){
-  days7[i]<-(rev(thurs[which(thurs$week==i),10])[1] - thurs[which(thurs$week==i),10][1])/
-    (rev(thurs[which(thurs$week==i),11])[1] - thurs[which(thurs$week==i),11][1])
-}
-
-plot(days1,pch=19,cex=1.4,col="cyan",ylim=c(0,20),xlab="Week",ylab="Speed (km/h)")
-points(days2,pch=19,cex=1.4, col="green",ylim=c(0,20))
-points(days3,pch=19,cex=1.4,col="pink",ylim=c(0,20))
-points(days4,pch=19,cex=1.4,col="yellow",ylim=c(0,20))
-points(days5,pch=19,cex=1.4,col="burlywood",ylim=c(0,20))
-points(days6,pch=19,cex=1.4,col="red",ylim=c(0,20))
-points(days7,pch=19,cex=1.4,col="orange",ylim=c(0,20))
-abline(h=8,lty=2)
-plot(days6,type="p",pch=19,ylim=c(0,2))
