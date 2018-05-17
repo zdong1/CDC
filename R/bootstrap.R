aa270cf6be05e62a62276cfc48b49bda23e4fa68
biggyd <zdong@Zhihangs-MacBook-Pro.local>
2018-05-17 07:32:20 -0700

    add an lct calculator

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -42,35 +42,40 @@ numeric_weekday<-function(daf){
 # mini is the minimum gap time you want to set up for your bootstrap simul.
 # maxi is the maximum gap time. Raise mini for a faster algorithm, decrease maxi for
 # higher accuracy
-newweek<-function(daf,mini,maxi){
-  temp_df<-daf[c(1:3,6:9,12,14)]
-  temp_df<-temp_df[which(temp_df$t.delta>mini & temp_df$t.delta<maxi),]
+newweek<-function(temp_df){
+  temp_df<-temp_df[which(temp_df$t.delta>0.02),]
+  temp_df<-temp_df[which(temp_df$t.delta<3),]
   # Give each weekname an assigned number, use the shrink version of dataset
   i = 1
   day.w<-NULL
   while (i < 8){
-    day.p <- temp_df[which(temp_df$wkdy==i),] # permutate a Mon/Tues/Weds...
-    day<-day.p[sample(nrow(day.p),1,replace = TRUE),]
-    j = 1
-    # Given a day, sample with replacement of a particular move until 24 hours are filled
-    while (j > 0){
-      day.s<-day.p[sample(nrow(day.p),1,replace = TRUE),]
-      day<- rbind(day.s,day)
-      if(sum(day$t.delta,na.rm=TRUE) < 24){
-        j = j+1
-      } else {
-        j = 0
-      }
-    }
-    if (i > 1){
-      day.w<-rbind(day.w,day)
+    if (nrow(temp_df[which(temp_df$wkdy==i),]) < 20){
+      i = i+1
     } else{
-      day.w<-day
+      day.p <- temp_df[which(temp_df$wkdy==i),] # permutate a Mon/Tues/Weds...
+      day<-day.p[sample(nrow(day.p),1,replace = TRUE),]
+      j = 1
+      # Given a day, sample with replacement of a particular move until 24 hours are filled
+      while (j > 0){
+        day.s<-day.p[sample(nrow(day.p),1,replace = TRUE),]
+        day<- rbind(day.s,day)
+        if(sum(day$t.delta,na.rm=TRUE) < 24){
+          j = j+1
+        } else {
+          j = 0
+        }
+      }
+      if (i > 1){
+        day.w<-rbind(day.w,day)
+      } else{
+        day.w<-day
+      }
+      i = i+1
     }
-    i = i+1
   }
   day.w
 }
+
 bsCalc <-function(df){
   df$t.delta[1] = 0
   df$dists[1] = 0
@@ -90,17 +95,41 @@ bsCalc <-function(df){
 # First, you must enumerate weekdays
 
 
+wk<-newweek(daf3)
 
 # This function generates a new week of a person x many times
-weeks <- replicate(20,newweek(daf30),simplify=FALSE)
-weeks_p30 <- rbindlist(weeks)
+weeks <- replicate(20,newweek(daf3),simplify=FALSE)
+wk3 <- rbindlist(weeks)
+week<-newweek(daaf33)
 # Calculate the Cumulative Speed Over Time for a newly generated week
 bt105<-bsCalc(weeks_p30)
 
 
 # Get the Plot, this is pretty raw at this point.
-plot(bt105$sum.t,bt105$s.vel, type="l",xlab="Cumulative Time",ylab="Cumulative Velocity",
+plot(bt104$sum.t,bt104$s.vel, type="l",xlab="Cumulative Time",ylab="Cumulative Velocity",
      lwd=1.2,col="brown",main="Bootstrapped Person Speed Plot")
 # TODO: Replicate this to hours
 # TODO: Refine the plot (5/16)
 
+
+# ============================================================
+# raw workspace
+# ============================================================
+
+lct<- NULL
+gender<-NULL
+age<-NULL
+id<-NULL
+df.lct<-data.frame(id,age,gender,lct)
+colnames(df.lct)<-c("id","gender","age","lct")
+
+get.lct<-function(df){
+  id<-df$personid[1]
+  lct<-df$sum.t[max(which(df$s.vel>rev(df$s.vel)[1]*1.1|df$s.vel<rev(df$s.vel)[1]*0.9))]
+  gender<-df$gen[1]
+  age<-df$age[1]
+  newrow<-c(id,gender,age,lct)
+  rbind(df.lct,newrow)
+}
+
+df.lct<-get.lct(bt182)
