9b5294c5cf537ff641fd7f1f2327956ac3929add
Zhihang Dong <zhdong2@gmail.com>
2018-11-13 17:05:05 -0800

    Update DistanceGen.R

--- R/DistanceGen.R
+++ R/DistanceGen.R
@@ -43,17 +43,17 @@ genDist<-function(raw){
   coordinates(raw)<- ~ long+lat
   proj4string(raw) <- CRS("+proj=longlat +datum=WGS84")
   class(raw@data$time)=c('POSIXt','POSIXct')
+  sort(raw@data$time)
+  raw@data$dists[1] = 0
+  raw@data$t.delta[1] = 0
+  raw@data$sum.dists[1] = 0
+  raw@data$sum.t[1] = 0
+  raw@data$vel[1] = 0
   for (i in 2:(nrow(raw))){
-    sort(raw@data$time)
-    raw@data$dists[1]<-0
     raw@data$dists[i]<-spDistsN1(pts= raw[i-1,], pt= raw[i,], longlat=TRUE)
-    raw@data$t.delta[1]<-0
-    raw@data$t.delta[i]<-difftime(raw@data$time[i],raw@data$time[i-1],units="hours")
-    raw@data$sum.dists[1]<-0
+    raw@data$t.delta[i]<-difftime(raw@data$time[i],raw@data$time[i-1],units="hours") 
     raw@data$sum.dists[i]<- raw@data$sum.dists[i-1]+raw@data$dists[i]
-    raw@data$sum.t[1]<-0
     raw@data$sum.t[i]<- raw@data$sum.t[i-1]+raw@data$t.delta[i]
-    raw@data$vel[1] <- 0
     raw@data$vel[i] <-raw@data$sum.dists[i]/raw@data$sum.t[i]
   }
   raw
